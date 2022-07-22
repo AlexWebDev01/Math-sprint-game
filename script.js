86762a9e1ebe09109283368adcffe87e78d01385
Alex Udodov <102874212+AlexWebDev01@users.noreply.github.com>
2022-07-22 16:07:43 +0300

    code review

--- script.js
+++ script.js
@@ -130,7 +130,7 @@ function checkTime() {
         // Correct Guess, No Penalty
       } else {
         // Incorrect Guess, Add Penalty
-        penaltyTime += 0.5;
+        penaltyTime += 5;
       }
     });
     finalTime = timePlayed + penaltyTime;
@@ -248,25 +248,27 @@ function populateGamePage() {
 
 // Displays 3, 2, 1, GO!
 function countdownStart() {
-  countdown.textContent = '3';
-  setTimeout(() => {
-    countdown.textContent = '2';
-  }, 1000);
-  setTimeout(() => {
-    countdown.textContent = '1';
-  }, 2000);
-  setTimeout(() => {
-    countdown.textContent = 'GO!';
-  }, 3000);
-}
+  let count = 3;
+  countdown.textContent = count;
+  const timeCountDown = setInterval(() => {
+    count--;
+    if (count === 0) {
+      countdown.textContent = 'Go';
+    } else if (count === -1) {
+      showGamePage();
+      clearInterval(timeCountDown);
+    } else {
+      countdown.textContent = count;
+    }
+  }, 1000)
+};
 
 // Navigate from Splash Page to CountdownPage to Game Page
 function showCountdown() {
   countdownPage.hidden = false;
   splashPage.hidden = true;
-  countdownStart();
   populateGamePage();
-  setTimeout(showGamePage, 4000);
+  countdownStart();
 }
 
 // Get the value from selected radio button
